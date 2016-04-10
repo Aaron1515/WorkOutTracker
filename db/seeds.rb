fb56e11f6321272cbda7d61ccfc0ab833bb05d3f
Aaron1515 <aaron1515@gmail.com>
2016-04-10 10:10:25 -0700

    fix model and seeded with false

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-User.create(name: "Eddy", email: "eddy@eddy.com", password: "password")
-Workout.create(name: "Bench Press", reps: 10, sets: 5,completed: true, user_id: 1, weight: 325)
-Workout.create(name: "Dips", reps: 12, sets: 5,completed: true, user_id: 1, weight: 200)
-Workout.create(name: "Shoulder Press", reps: 10, sets: 5,completed: true, user_id: 1, weight: 150)
+eddy = User.create(name: "Eddy", email: "eddy@eddy.com", password: "password")
+# eddy = User.find(1)
+
+
+
+
+eddy.workouts.create(name: "Bench Press", reps: 10, sets: 5,completed: true, weight: 325)
+eddy.workouts.create(name: "Dips", reps: 12, sets: 5,completed: false, weight: 200)
+eddy.workouts.create(name: "Shoulder Press", reps: 10, sets: 5,completed: true, weight: 150)
