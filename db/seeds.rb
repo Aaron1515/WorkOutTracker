334aeeb88bcbfc2880f081dd7d67302a905e6059
Aaron1515 <aaron1515@gmail.com>
2016-04-17 14:44:27 -0700

    create view page just for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 eddy = User.create(name: "Eddy", email: "eddy@eddy.com", password: "password", admin: true)
-# eddy = User.find(1)
+aaron = User.create(name: "Aaron", email: "aaron@aaron.com", password: "password", admin: false)
 
 
-
-
-eddy.workouts.create(name: "Bench Press", reps: 10, sets: 5,completed: true, weight: 325)
-eddy.workouts.create(name: "Dips", reps: 12, sets: 5,completed: false, weight: 200)
-eddy.workouts.create(name: "Shoulder Press", reps: 10, sets: 5,completed: true, weight: 150)
+aaron.workouts.create(name: "Bench Press", reps: 10, sets: 5,completed: true, weight: 325)
+aaron.workouts.create(name: "Dips", reps: 12, sets: 5,completed: false, weight: 200)
+aaron.workouts.create(name: "Shoulder Press", reps: 10, sets: 5,completed: true, weight: 150)
