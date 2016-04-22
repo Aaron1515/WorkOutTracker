879cbd2f557bc06d3fa87cbd84eab02829dbe61a
schaui6 <sanderfer.chau@gmail.com>
2016-04-22 12:02:27 -0700

    Fix Workouts create

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -6,14 +6,12 @@ class WorkoutsController < ApplicationController
   def new
     @workout = Workout.new
     @user = User.find_by(id: params[:user_id])
+    @workout.user_id = @user.id
   end
 
   def create
-    p '%%%%%%%%%' * 100
-    @workout = params[:workout][:workout]
-    p @workout[:name]
-    p '%%%%%%%%%' * 100
-    @workout = Workout.new(name: @workout[:name],sets: @workout[:sets], reps: @workout[:reps], weight: @workout[:weight],user_id: @workout[:user_id])
+    @workout = Workout.new(workout_params)
+    @workout.user_id = params[:user_id]
     if @workout.save
       redirect_to users_path
     else
@@ -30,6 +28,6 @@ class WorkoutsController < ApplicationController
   private
 
   def workout_params
-    params.require(:workout).permit(:name, :sets, :reps, :weight, :user_id)
+    params.require(:workout).permit(:name, :sets, :reps, :weight)
   end
 end
