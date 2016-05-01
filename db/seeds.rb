cf9b3c13924ab6ab2b81b82a5bbd01af3d07ab58
Aaron1515 <aaron1515@gmail.com>
2016-05-01 16:30:51 -0700

    add 30 test users for testing delete features

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,7 @@ sanderfer.workouts.create(name: "Mtn Climbers", reps: 10, sets: 3, weight: "BW",
 sanderfer.workouts.create(name: "From Jumps", reps: 10, sets: 3, weight: "BW", phase: 1, day:1, rest:'0', completed: false)
 sanderfer.workouts.create(name: "Sit-Ups", reps: 10, sets: 3, weight: "BW", phase: 1, day:1, rest:'0', completed: false)
 
+
+30.times do
+random_user = User.create(name: "Aaron", email: "aaron@aaron.com" + rand(99).to_s, password: "password", admin: false)
+end
