97906ff38e1b2b6b44329c67c38c58ff695d8814
Aaron1515 <aaron1515@gmail.com>
2016-05-02 14:39:38 -0700

    create success message when workout is saved to data base

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -25,6 +25,7 @@ class WorkoutsController < ApplicationController
     @workout = Workout.new(workout_params)
     @workout.user_id = params[:user_id]
     if @workout.save
+      flash[:success] = "Workout successfully created"
       redirect_to user_path(params[:user_id])
     else
       render 'new'
