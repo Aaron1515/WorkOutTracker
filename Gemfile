ed6b90818b92d1459c9f600c4dec1502d95a6692
Aaron1515 <aaron1515@gmail.com>
2016-05-03 00:47:59 -0700

    add faker gem

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bcrypt', '~> 3.1.7'
 gem 'autoprefixer-rails'
 gem 'rails_12factor', group: :production
+gem 'faker'
 
 group :development, :test do
   gem 'byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ end
 # sanderfer = User.create(name: "Sanderfer", email: "san@san.com", password: "password", admin: false)
 
 30.times do
-  random_user = User.create(name: "Aaron", email: "aaron@aaron.com" + rand(9999).to_s, password: "password", admin: false)
+  random_user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: "password", admin: false)
     10.times do
       random_user.workouts.create(name: rand_workout, reps: rand(7..10), sets: rand(2..3), weight: rand_workout, phase: rand(1..4), day: rand(1..4), rest: rand(0..3))
       aaron.workouts.create(name: rand_workout, reps: rand(7..10), sets: rand(2..3), weight: rand_workout, phase: rand(1..4), day: rand(1..4), rest: rand(0..3))
