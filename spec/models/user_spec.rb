c94c2369263966682e6c4db30e8fce65a323057a
Aaron1515 <aaron1515@gmail.com>
2016-05-27 17:56:09 -0700

    add test with let

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,32 +2,50 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it "is valid with a name, email, and password" do
-    user = User.new(
-      name: 'Aaron',
-      email: 'aaron@gmail.com', 
-      password: 'password')
-    expect(user).to be_valid
+  # it "is valid with a name, email, and password" do
+  #   user = User.new(
+  #     name: 'Aaron',
+  #     email: 'aaron@aaron.com',
+  #     password: 'password')
+  #   expect(user).to be_valid
+  # end
+  describe 'Creating new user' do
+    let(:user) { User.new( name: 'Aaron', email: 'aaron@aaron.com',password: 'password') }
+
+    it 'is valid with name, email, password' do
+      expect(user.save).to equal(true)
+    end
+
+    it 'is valid with name equal to "Aaron"' do
+      expect(user.name).to include("Aaron")
+    end
+    it 'is valid with name equal to "aaron@aaron.com"' do
+      expect(user.email).to include("aaron@aaron.com")
+    end
+    it 'is valid with name equal to Hash String' do
+      expect(user.password).to be_a_kind_of (String)
+    end
   end
 
+
   it "is invalid without a name" do
-    user = User.new(name: nil)
+    user = User.new(name: nil, email: 'aaron@aaron.com', password: 'password')
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
   end
-  
+
   it "is invalid without a password" do
     user = User.new(password: nil)
     user.valid?
     expect(user.errors[:password]).to include("can't be blank")
   end
-  
+
   it "is invalid without an email address" do
     user = User.new(email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
-  
+
   it "is invalid with a duplicate email address" do
     User.create(
       name: 'Joe',
@@ -45,10 +63,10 @@ RSpec.describe User, type: :model do
     user = User.new(name: 'Joe')
     expect(user.name).to eq 'Joe'
   end
-  
+
   it "returns a contact's email as a string" do
         user = User.new(email: 'joe@joe.com')
     expect(user.email).to eq 'joe@joe.com'
   end
-  
+
 end
