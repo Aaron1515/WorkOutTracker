58d4c778e1179f99a4d42523f1596302a43766c0
Aaron1515 <aaron1515@gmail.com>
2016-06-04 21:56:34 -0700

    update spec for pending

--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe WorkoutsController, type: :controller do
+  let(:user) { User.create( name: 'Aaron', email: 'aaron@aaron.com',password: 'password') }
+
+  describe 'Get #index' do
+  end
+  describe 'Get #show' do
+  end
+  describe 'Get #edit' do
+  end
+  describe 'Get #update' do
+  end
+  describe 'Get #destroy' do
+  end
 
 end
