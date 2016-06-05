1e22262a839c26e1083df592a599156a461e3b3a
Aaron1515 <aaron1515@gmail.com>
2016-06-04 20:53:10 -0700

    add users controller rspec

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  # describe 'Get #index' do
+  #     context 'when user is logged in' do
+  #     with :user
+  #     before do
+  #       sign_in user
+  #       get :index
+  #     end
+  #     it { is_expected.to respond_with :ok }
+  #     it { is_expected.to respond_with_content_type :html }
+  #     it { is_expected.to render_with_layout :application }
+  #     it { is_expected.to render_template :index }
+  #   end
+
+  # end
+
+
+end
